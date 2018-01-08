5264c9eebff9abfae85e86b6e05cedceaf21256a
iLikeTree <1220862922@qq.com>
2018-01-08 17:10:25 +0800

    update

--- demo/demo.js
+++ demo/demo.js
@@ -1,38 +1,47 @@
-(function(){
-    $(document).on('mousewheel', function(event) {
+(function () {
+    $(document).on('mousewheel', function (event) {
         throttle(listenScroll, event);
     });
+    var ids = ['home', 'my', 'demo', 'daily', 'end'];
     var activeId = 0;
+    var len = ids.length;
     var done = true;
     // 页面滚动事件
     function listenScroll(event) {
-        if(done) {
-            if(event.deltaY > 0) {
-                if(activeId === 0) return;
+        if (done) {
+            var oldId = -1;
+            if (event.deltaY > 0) {
+                if (activeId === 0) return;
+                oldId = activeId;
                 activeId--;
             } else {
-                if(activeId === 4) return;
+                if (activeId === len - 1) return;
+                oldId = activeId;
                 activeId++;
             }
             done = false;
-            document.body.className = 'active-' + activeId;
-            setTimeout(function() {
+            $('body').removeClass('active-' + ids[oldId]).addClass('active-' + ids[activeId]);
+            $('.sec-' + ids[oldId]).addClass('sec-out').removeClass('sec-in');
+            $('.sec-' + ids[activeId]).addClass('sec-in').removeClass('sec-out');
+            $('#slidebar li').removeClass('slidebar-active').eq(activeId).addClass('slidebar-active');
+            setTimeout(function () {
                 done = true;
             }, 1000);
         }
-        
+
     }
 
     // 函数节流
     function throttle(method, context) {
         clearTimeout(method.tId);
-        method.tId = setTimeout(function(){
+        method.tId = setTimeout(function () {
             method(context);
         }, 100);
     }
 
-    $(function(){
-       document.body.className = 'active-0';
-       $('#media').addClass('show'); 
+    $(function () {
+        $('body').addClass('active-' + ids[activeId]);
+        $('.sec-' + ids[activeId]).addClass('sec-in');
+        $('#media').addClass('show');
     });
 })(jQuery);
